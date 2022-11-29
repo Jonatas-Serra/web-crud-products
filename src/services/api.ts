fd552412226255580c46984efb3ad102728c2467
Jonatas Serra <jonatasserra@outlook.com>
2022-11-28 23:43:10 -0300

    refactor(api): set new api deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://apicrudproducts.up.railway.app↗',
+  baseURL: 'https://apicrudproducts.up.railway.app',
 })
 
 export default api
